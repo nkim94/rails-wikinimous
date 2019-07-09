ba3920bb55f1823e830ea3a39971ed850b440859
Nathan Kim <nakim994@gmail.com>
2019-07-09 15:04:09 +0200

    created a wiki with a nice front page and basic inner pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index'
+  post 'articles', to: 'articles#create'
+
+  get 'articles/new', as: :new_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
 end
